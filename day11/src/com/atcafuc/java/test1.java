4ae4d7e18c5ce2c10acaa40be60809546c2b9f88
jianhao <jianhao@123.com>
2021-08-30 16:06:09 +0800

    master commit2 java code

--- day11/src/com/atcafuc/java/test1.java
+++ day11/src/com/atcafuc/java/test1.java
@@ -8,5 +8,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hot-fix");
+        System.out.println("master1");
     }
 }
